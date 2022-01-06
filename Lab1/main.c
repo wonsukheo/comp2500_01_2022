cb102286d52802007d922cd7bc2809c92d794e6f
wonsukheo <wonsukheo8@gmail.com>
2022-01-06 00:19:55 -0600

    lab1 1st_commit

--- Lab1/main.c
+++ Lab1/main.c
@@ -1,4 +1,12 @@
+#include <stdio.h>
+
+#include "add.h"
+
 int main(void)
 {
+    int singed_int = -1024;
+    unsigned int unsigned_int1 = 394;
+    unsigned int unsigned_int2 = 2,147,483,647;
+
     return 0;
 }
