663c21907f4f612ff8ebec9a0f8ce606c1342e09
wonsukheo <wonsukheo8@gmail.com>
2022-01-17 14:36:46 -0600

    few fix before 1st buildbot test

--- Lab3/cyber_spaceship.c
+++ Lab3/cyber_spaceship.c
@@ -1,12 +1,15 @@
 #include "cyber_spaceship.h"
 
+#define TRUE (1)
+#define FALSE (0)
+#define NULL ((void*)0)
 
 const char* get_longest_safe_zone_or_null(const char* const cab_start_location, const size_t cab_length, const char* const cluster_start_locations[], const size_t cluster_lengths[], const size_t cluster_count, size_t* out_longest_safe_area_length)
 {
     const char* travel_ptr = cab_start_location;
     const char* longest_safe_zone_start_ptr = NULL;
 
-    size_t bSafe = TRUE;
+    size_t bSafe = FALSE;
     size_t safe_area_length = 0;
     size_t longest_safe_area_length = 0;
     size_t overlapped_cluster_cnt = 0;
@@ -29,21 +32,18 @@ const char* get_longest_safe_zone_or_null(const char* const cab_start_location, 
         
         for (i = 0; i < cluster_count; ++i) {
             if (travel_ptr == cluster_start_locations[i]) {
-
                 overlapped_cluster_cnt++;
-
             } else if (travel_ptr == cluster_start_locations[i] + cluster_lengths[i] - 1) {
-
                 overlapped_cluster_cnt--;
             }
         }
 
-        if (overlapped_cluster_cnt % 2 == 0) {      
-   
-            bSafe = TRUE;
+        if (overlapped_cluster_cnt % 2 == 0) {     
+            if (bSafe) { 
+                safe_area_length++;     
+            }
 
-            safe_area_length++;
-
+            bSafe = TRUE;       
         } else if (bSafe) {          
 
             if (safe_area_length >= longest_safe_area_length) {
@@ -69,20 +69,15 @@ int get_travel_time(const char* const cab_start_location, const size_t cab_lengt
     const double SAFE_AREA_TRAVEL_SPEED = 1 / 6.0;
     const double UNSAFE_AREA_TRAVEL_SPEED = 1 / 12.0;
 
-    const char* travel_ptr = cab_start_location;
-    double travel_time;
-
+    const char* travel_ptr = cab_start_location;    
     size_t safe_area_length = 0;
     size_t unsafe_area_length = 0;
-
     size_t overlapped_cluster_cnt = 0;
 
     /* cab_length > 0 expected */
   
     if (cluster_count == 0) {
-        travel_time = cab_length / 6.0;
-  
-        return (int)travel_time;
+        return (int)(cab_length * SAFE_AREA_TRAVEL_SPEED + 0.5);
     }
 
     while (travel_ptr < cab_start_location + cab_length) {
@@ -90,11 +85,8 @@ int get_travel_time(const char* const cab_start_location, const size_t cab_lengt
         
         for (i = 0; i < cluster_count; ++i) {
             if (travel_ptr == cluster_start_locations[i]) {
-
                 overlapped_cluster_cnt++;
-
             } else if (travel_ptr == cluster_start_locations[i] + cluster_lengths[i] - 1) {
-
                 overlapped_cluster_cnt--;
             }
         }
@@ -108,7 +100,6 @@ int get_travel_time(const char* const cab_start_location, const size_t cab_lengt
         ++travel_ptr;
     }
   
-    travel_time = safe_area_length * SAFE_AREA_TRAVEL_SPEED + unsafe_area_length * UNSAFE_AREA_TRAVEL_SPEED;
-
-    return (int)travel_time;
+    return (int)(safe_area_length * SAFE_AREA_TRAVEL_SPEED + unsafe_area_length * UNSAFE_AREA_TRAVEL_SPEED + 0.5);
 }
+
