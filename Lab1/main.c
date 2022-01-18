da5be6f7cb1f87752586d1a81718b46e3bbea410
wonsukheo8 <wonsukheo8@gmail.com>
2022-01-18 00:31:50 -0600

    no message

--- Lab1/main.c
+++ Lab1/main.c
@@ -1,4 +1,21 @@
+#include <stdio.h>
+
+enum day {
+    DAY_M,
+    DAY_T = 10
+};
+
 int main(void)
 {
+    int x = 3;
+
+    switch (x) {
+    case 0:
+    case 1:
+        ;
+    }
+ 
     return 0;
 }
+
+
