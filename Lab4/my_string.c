66e49ca163798913461fb8dfe8fe559a0a27dd98
wonsukheo <wonsukheo8@gmail.com>
2022-01-30 11:26:14 -0600

    *improve readability *no logic changed

--- Lab4/my_string.c
+++ Lab4/my_string.c
@@ -2,8 +2,6 @@
 
 #define NULL ((void*)0)
 
-/* should we do null check? */
-
 size_t my_strlen(const char* str)
 {
     const char* str_ptr = str;
@@ -20,25 +18,26 @@ size_t my_strlen(const char* str)
 
 void reverse(char* str)
 {
+    char* str_ptr = str;
     char* str_end = str + my_strlen(str) - 1;
 
     if (str == NULL) {
         return;
     }
 
-    while (str_end > str) {
-        char c = *str;
-        *str = *str_end;
+    while (str_end > str_ptr) {
+        char c = *str_ptr;
+        *str_ptr = *str_end;
         *str_end = c;
         
-        ++str;
+        ++str_ptr;
         --str_end;
     }
 }
 
 int index_of(const char* str, const char* word)  
 {
-    const char* str_start = str;
+    const char* str_ptr = str;
 
     if (str == NULL || word == NULL) {
         return -1;
@@ -48,51 +47,50 @@ int index_of(const char* str, const char* word)
         return 0;
     }
 
-    while (*str != '\0') {
-        if (*str == *word) {
-            const char* str_ptr = str;
+    while (*str_ptr != '\0') {
+        if (*str_ptr == *word) {
+            const char* p = str_ptr;
             const char* word_ptr = word;  
 
             while (*++word_ptr != '\0') {
-                if (*word_ptr != *++str_ptr) {
+                if (*word_ptr != *++p) {
                     break;
                 }
             }       
                 
             if (*word_ptr == '\0') {
-                return str - str_start;
+                return str_ptr - str;
             }   
         }
         
-        ++str;
+        ++str_ptr;
     }    
 
     return -1;
 }
 
-/* why no !? */
-
 void reverse_by_words(char* str) 
 {
     char* str_ptr = str;
+    char* str_start = str;
 
-    if (str == NULL) {
+    if (str_ptr == NULL) {
         return;
     }
  
-    while (*str != '\0') {
-        if (*str == ' ') {
-            *str = '\0';
-            reverse(str_ptr);
+    while (*str_ptr != '\0') {
+        if (*str_ptr == ' ') {
+            *str_ptr = '\0';
+            reverse(str_start);
             
-            *str = ' ';
-            str_ptr = str + 1;
+            *str_ptr = ' ';
+            str_start = str_ptr + 1;
         }
         
-        ++str;
+        ++str_ptr;
     }
     
-    reverse(str_ptr);  
+    reverse(str_start);
 }
 
 char* tokenize(char* str_or_null, const char* delims)
@@ -133,8 +131,6 @@ char* tokenize(char* str_or_null, const char* delims)
     
     return str_ptr;
 }
-
-/* check if block scope static var can have same name */
 
 char* reverse_tokenize(char* str_or_null, const char* delims)
 {
