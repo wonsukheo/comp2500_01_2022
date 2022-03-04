6c3b06fac96ffee238668db466f9dbf352910632
wonsukheo <wonsukheo8@gmail.com>
2022-03-04 02:30:26 -0600

    1st try

--- Lab7/tokenize.c
+++ Lab7/tokenize.c
@@ -2,50 +2,42 @@
 
 #include "tokenize.h"
 
-int my_strlen(const char* str)
-{
-    const char* p = str;
-
-    while (*p++ != '\0') {
-    }
-    
-    return p - str - 1;
-}
-
 char** tokenize_malloc(const char* str, const char* delim)
 {
     const char* str_start_p = str;
     const char* str_p = str;
-    size_t current_token = 0;
-    size_t max_token = 0;
-    char* tokens[] = NULL;
-    char** tokens_ptr = NULL;
+
+    char** tokens = NULL;
+    char** tokens_p = NULL;
+   
+    size_t i = 0;
 
     while (*str_p != '\0') {
         const char* delim_p = delim;
   
         while (*delim_p != '\0') {
             if (*str_p == *delim_p) {
-                char* temp_str = malloc(*str_p - str_start_p + 1);
-                strncpy(temp_str, str_start_p, 
-                
-                
+                size_t len;
+                char* pa_str;
+
                 if (str_p == str_start_p) {
                     str_start_p = str_p + 1;
 
                     break;
                 }    
+ 
+                len = str_p - str_start_p;
+                pa_str = malloc(len);
 
-                if (current_token == max_token) {                    
-                    tokens_ptr = realloc(tokens, sizeof(const char*) * ++max_token);
-                    
-                    if (tokens_ptr == NULL) {
-                    }
-           
-                    tokens = tokens_ptr;
-                }
+                strncpy(pa_str, str_start_p, len);
 
-                tokens[current_token++] = str_start_p;
+                pa_str[len] = '\0';             
+               
+                tokens_p= (char**) realloc(tokens, sizeof(const char*) * ++i);
+
+                tokens = tokens_p;
+
+                tokens[i - 1] = pa_str;
                 
                 str_start_p = str_p + 1;          
             }    
@@ -56,14 +48,11 @@ char** tokenize_malloc(const char* str, const char* delim)
         ++str_p;
     }    
     
-    tokens_ptr = realloc(tokens, sizeof(const char*) * ++max_token);
+    tokens_p = (char**) realloc(tokens, sizeof(const char*) * ++i);
 
-    if (tokens_ptr == NULL) {
-    }
+    tokens = tokens_p;
 
-    tokens = tokens_ptr;
-
-    tokens[current_token] = NULL;
+    tokens[i - 1] = NULL;
     
     return tokens;
 }
