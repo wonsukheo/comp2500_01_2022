6f1f48c0e53581b550848123cc194b1f47db703d
wonsukheo <wonsukheo8@gmail.com>
2022-03-17 21:16:53 -0500

    fixing range

--- Lab9/macros.h
+++ Lab9/macros.h
@@ -7,9 +7,11 @@
 
 #define ABS(x) ((x) < 0 ? -(x) : x)
 
-#define RANGE(curr, min, max) for (curr = min; (curr <= max); ++curr) 
+#define RANGE(curr, min, max) if (min <= max)                           \ 
+                              for (curr = (min); curr <= (max); ++curr) \
 
-#define RANGE_DESC(curr, max, min) for (curr = max; (curr >= min); --curr) 
+#define RANGE_DESC(curr, max, min) if (max >= min)                           \
+                                   for (curr = (max); curr >= (min); --curr) \ 
 
 #define SET(ary, start, count, value) do { size_t i = start;    \
                                       while (i < start + count) \
