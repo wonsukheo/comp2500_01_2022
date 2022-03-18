3a0358ac422ee57f5e2a87a47e349bf598b8fe2a
wonsukheo <wonsukheo8@gmail.com>
2022-03-17 20:28:21 -0500

    added parenthesis

--- Lab9/macros.h
+++ Lab9/macros.h
@@ -1,15 +1,15 @@
 #ifndef MACROS_H
 #define MACROS_H
 
-#define MAX(x, y) (x < y ? y : x)
+#define MAX(x, y) (((x) < (y)) ? (y) : (x))
 
-#define MIN(x, y) (x < y ? x : y)
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
-#define ABS(x) (x < 0 ? -x : x)
+#define ABS(x) (((x) < 0) ? (-x) : (x)))
 
-#define RANGE(curr, min, max) for (curr = min; curr <= max; ++curr) 
+#define RANGE(curr, min, max) for (curr = min; (curr <= max); ++curr) 
 
-#define RANGE_DESC(curr, max, min) for (curr = max; curr >= min; --curr) 
+#define RANGE_DESC(curr, max, min) for (curr = max; (curr >= min); --curr) 
 
 #define SET(ary, start, count, value) do { size_t i = start;    \
                                       while (i < start + count) \
